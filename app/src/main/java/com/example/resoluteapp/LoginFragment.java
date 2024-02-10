c75794b7e423027013fb5d22c81cd920733cb4bd
agentry5 <agentry5@emich.edu>
2024-02-10 01:32:14 -0500

    feat: Add username/password entry
    This commit added the "login_username_entry" and "login_password_entry" elements to the fragment_login.xml, and added getText() functionality for those textboxes in LoginFragment.java. Additionally, the login screen's UI was redesigned a bit to show the app's name, and to space textboxes and buttons appropriately.
    
    Additionally, two Toast messages displaying the entered Username and Password were prepared, for testing.
    
    *Updated UI: fragment_login.xml
    *Added functionality: LoginFragment.java
    *Testing functionality: LoginFragment.java lines 50 to 59
    
    Addresses #10

--- app/src/main/java/com/example/resoluteapp/LoginFragment.java
+++ app/src/main/java/com/example/resoluteapp/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.example.resoluteapp;
 
+import android.app.Activity;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +10,8 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.resoluteapp.databinding.FragmentFirstBinding;
 import com.example.resoluteapp.databinding.FragmentLoginBinding;
@@ -36,6 +39,26 @@ public class LoginFragment extends Fragment {
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //Username and Password EditText items
+                EditText usernameET = (EditText)getActivity().findViewById(R.id.login_username_entry);
+                EditText passwordET = (EditText)getActivity().findViewById(R.id.login_password_entry);
+
+                //Get entered username and password
+                String usernameString = usernameET.getText().toString();
+                String passwordString = passwordET.getText().toString();
+
+                //TESTING CODE START FOR COMMIT
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast1 = Toast.makeText(getActivity().getApplicationContext(), "Username is: "+usernameString, duration);
+                toast1.show();
+
+                Toast toast2 = Toast.makeText(getActivity().getApplicationContext(), "Password is: "+passwordString, duration);
+                toast2.show();
+
+                //TESTING CODE END FOR COMMIT
+
+                //Navigate to home screen
                 NavHostFragment.findNavController(LoginFragment.this)
                         .navigate(R.id.action_loginFragment_to_homeFragment);
             }
