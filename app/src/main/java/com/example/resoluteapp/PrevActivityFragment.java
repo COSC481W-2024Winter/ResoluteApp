8b7c43b31e6db702fc7c8eefc4b02a5a4262b746
Victoria Ceci <145465524+vceci@users.noreply.github.com>
2024-02-14 11:14:16 -0500

    feat: UI layout and display exercises from Firestore
    This commit sets up the layout for the previous exercise page with a table and 4 columns for each attribute of the exercise log. It also calls to Firestore to retrieve the dummy exercise data and displays them appropriately in the table.
    
    * added code to the fragment_prev_activity.xml file for the UI layout
    * added function to the PrevActivityFragment.java file for retrieving information from Firestore
    * fills the table with the data from Firestore in the onCreateView function in PrevActivityFragment.java
    
    issue #12

--- app/src/main/java/com/example/resoluteapp/PrevActivityFragment.java
+++ app/src/main/java/com/example/resoluteapp/PrevActivityFragment.java
@@ -1,5 +1,6 @@
 package com.example.resoluteapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,8 +10,20 @@ import androidx.navigation.fragment.NavHostFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.example.resoluteapp.databinding.FragmentPrevActivityBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.common.collect.Table;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 /**
@@ -22,6 +35,8 @@ public class PrevActivityFragment extends Fragment {
 
     private FragmentPrevActivityBinding binding;
 
+    FirebaseFirestore DB = FirebaseFirestore.getInstance();
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -29,8 +44,16 @@ public class PrevActivityFragment extends Fragment {
     ) {
 
         binding = FragmentPrevActivityBinding.inflate(inflater, container, false);
-        return binding.getRoot();
 
+        try {
+            fillTable();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+        return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -44,6 +67,73 @@ public class PrevActivityFragment extends Fragment {
                         .navigate(R.id.action_prevActivityFragment_to_homeFragment);
             }
         });
+    }
+
+    public void fillTable() throws InterruptedException, ExecutionException {
+
+        DB.collection("victoria_exercises").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                        if (!queryDocumentSnapshots.isEmpty()) {
+
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            TableLayout tl = (TableLayout) getView().findViewById(R.id.tableLayout);
+                            int tableWidth = tl.getWidth() / 4;
+
+                            tl.removeViews(1, Math.max(0, tl.getChildCount() - 1));
+
+
+                            for (DocumentSnapshot d : list) {
+                                String date = d.getString("date");
+                                String exercise = d.getString("exercise");
+                                String amount = d.getString("amount");
+                                String unit = d.getString("unit");
+
+                                TableRow tr = new TableRow(getActivity().getApplicationContext());
+                                TextView tv1 = new TextView(getActivity().getApplicationContext());
+                                TextView tv2 = new TextView(getActivity().getApplicationContext());
+                                TextView tv3 = new TextView(getActivity().getApplicationContext());
+                                TextView tv4 = new TextView(getActivity().getApplicationContext());
+
+                                tv1.setText(date);
+                                tv1.setTextColor(Color.BLACK);
+                                tv1.setPadding(10, 10, 10, 10);
+                                tv1.setTextSize(12);
+                                tv1.setWidth(tableWidth);
+
+                                tr.addView(tv1, 0);
+
+                                tv2.setText(exercise);
+                                tv2.setTextColor(Color.BLACK);
+                                tv2.setPadding(10, 10, 10, 10);
+                                tv2.setTextSize(12);
+                                tv2.setWidth(tableWidth);
+
+                                tr.addView(tv2, 1);
+
+                                tv3.setText(amount);
+                                tv3.setTextColor(Color.BLACK);
+                                tv3.setPadding(10, 10, 10, 10);
+                                tv3.setTextSize(12);
+                                tv3.setWidth(tableWidth);
+
+                                tr.addView(tv3, 2);
+
+                                tv4.setText(unit);
+                                tv4.setTextColor(Color.BLACK);
+                                tv4.setPadding(10, 10, 10, 10);
+                                tv4.setTextSize(12);
+                                tv4.setWidth(tableWidth);
+
+                                tr.addView(tv4, 3);
+
+                                tl.addView(tr);
+                            }
+                        }
+                    }
+                });
     }
 
     @Override
