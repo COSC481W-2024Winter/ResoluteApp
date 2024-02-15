0b24353f0b887e22ddc5b579da02d03a2453ed33
Kailin Bedoway <kbedoway@emich.edu>
2024-02-14 22:39:47 -0500

    feat: Friends page UI and sending friend requests to Firestore
    This commit sets up the layout for the friends page with a text box and a button for sending friend requests. It will query dummy data in Firestore, and send dummy data as well.
    
    * added to fragment_friends.xml for UI layout
    * added to FriendsFragment.java for sending friend requests to Firestore
    * sends friend request to requests collection and request collection of entered username in Firestore
    
    issue #11

--- app/src/main/java/com/example/resoluteapp/FriendsFragment.java
+++ app/src/main/java/com/example/resoluteapp/FriendsFragment.java
@@ -6,16 +6,30 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.resoluteapp.databinding.FragmentFriendsBinding;
 import com.example.resoluteapp.databinding.FragmentProfileBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FriendsFragment#newInstance} factory method to
+ * Use the {@link FriendsFragment newInstance} factory method to
  * create an instance of this fragment.
  */
 public class FriendsFragment extends Fragment {
@@ -35,6 +49,82 @@ public class FriendsFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        //Send Request Button
+        binding.sendRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get current time
+                LocalDateTime time = LocalDateTime.now();
+                ZonedDateTime zdt = time.atZone(ZoneId.of("America/Detroit"));
+                String currentTime = DateTimeFormatter.ofPattern("M/d/yy - h:mm:ss").format(zdt);
+
+                //Get sending user
+                String sendingUsername = "TEST";
+
+                //Get requested user
+                EditText enteredUsername = (EditText)getActivity().findViewById(R.id.enter_username);
+                String requestedUsername = enteredUsername.getText().toString();
+
+                FirebaseFirestore DB = FirebaseFirestore.getInstance();
+
+                //Search users collection to see if requestedUsername exists
+                DB.collection("users")
+                        .whereEqualTo("username", requestedUsername)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                //If requestedUsername exists, send sendingUsername to requests collection and collection of requests_[requestedUsername]
+                                if (task.getResult().size() == 1) {
+                                    //Create data to be stored in Firestore
+                                    Map<String, Object> data = new HashMap<>();
+                                    data.put("sent", currentTime);
+                                    data.put("username", sendingUsername);
+
+                                    //Add data to Firestore requests collection
+                                    DB.collection("requests")
+                                            .add(data)
+                                            .addOnSuccessListener(documentReference -> {
+                                                // Data addition successful
+                                                Log.d("Firestore", "Data added successfully");
+                                            })
+                                            .addOnFailureListener(e -> {
+                                                // Handle errors
+                                                Log.e("Firestore", "Error adding data: " + e.getMessage());
+                                            });
+
+                                    //Add data to Firestore requests_[requestedUsername] collection
+                                    DB.collection("requests_" + requestedUsername)
+                                            .add(data)
+                                            .addOnSuccessListener(documentReference -> {
+                                                // Data addition successful
+                                                Log.d("Firestore", "Data added successfully");
+                                            })
+                                            .addOnFailureListener(e -> {
+                                                // Handle errors
+                                                Log.e("Firestore", "Error adding data: " + e.getMessage());
+                                            });
+
+                                    //Show "Request sent" Toast
+                                    Toast toast = Toast.makeText(getActivity().getApplicationContext(), "Request sent", Toast.LENGTH_SHORT);
+                                    toast.show();
+                                }
+
+                                //If requestedUsername does not exist, no request is sent
+                                else {
+                                    //Show "Username not found" Toast
+                                    Toast toast = Toast.makeText(getActivity().getApplicationContext(), "Username not found", Toast.LENGTH_SHORT);
+                                    toast.show();
+                                }
+
+                                //Reset enter_username text box
+                                EditText resetText = (EditText)getActivity().findViewById(R.id.enter_username);
+                                resetText.setText("");
+                            }
+                        });
+            }
+        });
 
         //Home Button
         binding.toHomeFromFriends.setOnClickListener(new View.OnClickListener() {
