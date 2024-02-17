86d2e659681c4102f1e666d21c190475aa7bc7de
Victoria Ceci <145465524+vceci@users.noreply.github.com>
2024-02-16 21:07:53 -0500

    fix: adds comments to java code
    This commit is to add comments throughout the java file to explain how the code is functioning.
    
    * added comments in the fillTable() function in PrevActivityFragment.java
    
    issue #12

--- app/src/main/java/com/example/resoluteapp/PrevActivityFragment.java
+++ app/src/main/java/com/example/resoluteapp/PrevActivityFragment.java
@@ -45,6 +45,7 @@ public class PrevActivityFragment extends Fragment {
 
         binding = FragmentPrevActivityBinding.inflate(inflater, container, false);
 
+        //calls the fillTable() function when the page is created so it is filled simultaneously
         try {
             fillTable();
         } catch (InterruptedException e) {
@@ -69,66 +70,83 @@ public class PrevActivityFragment extends Fragment {
         });
     }
 
+    //function to fill the table with data from exercise collection
     public void fillTable() throws InterruptedException, ExecutionException {
 
+        //indicates what collection to retrieve data from
         DB.collection("victoria_exercises").get()
                 .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
 
+                        //checks that there is currently data to retrieve
                         if (!queryDocumentSnapshots.isEmpty()) {
 
+                            //puts all the documents of data into a list
                             List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+
+                            //finds the table in the fragment_prev_activity.xml
                             TableLayout tl = (TableLayout) getView().findViewById(R.id.tableLayout);
-                            int tableWidth = tl.getWidth() / 4;
+                            int columnWidth = tl.getWidth() / 4;
 
                             tl.removeViews(1, Math.max(0, tl.getChildCount() - 1));
 
-
+                            //loops through all the documents in the list to fill the table
                             for (DocumentSnapshot d : list) {
+                                //puts each piece of data in the document into its own variable
                                 String date = d.getString("date");
                                 String exercise = d.getString("exercise");
                                 String amount = d.getString("amount");
                                 String unit = d.getString("unit");
 
+                                //creates a new row for the exercise and 4 text views in the row
                                 TableRow tr = new TableRow(getActivity().getApplicationContext());
                                 TextView tv1 = new TextView(getActivity().getApplicationContext());
                                 TextView tv2 = new TextView(getActivity().getApplicationContext());
                                 TextView tv3 = new TextView(getActivity().getApplicationContext());
                                 TextView tv4 = new TextView(getActivity().getApplicationContext());
 
+                                //sets the first text view with the date data for the exercise
                                 tv1.setText(date);
                                 tv1.setTextColor(Color.BLACK);
                                 tv1.setPadding(10, 10, 10, 10);
                                 tv1.setTextSize(12);
-                                tv1.setWidth(tableWidth);
+                                tv1.setWidth(columnWidth);
 
+                                //adds it to the table row in the first column
                                 tr.addView(tv1, 0);
 
+                                //sets the second text view with the exercise data for the exercise
                                 tv2.setText(exercise);
                                 tv2.setTextColor(Color.BLACK);
                                 tv2.setPadding(10, 10, 10, 10);
                                 tv2.setTextSize(12);
-                                tv2.setWidth(tableWidth);
+                                tv2.setWidth(columnWidth);
 
+                                //adds it to the table row in the second column
                                 tr.addView(tv2, 1);
 
+                                //sets the third text view with the amount data for the exercise
                                 tv3.setText(amount);
                                 tv3.setTextColor(Color.BLACK);
                                 tv3.setPadding(10, 10, 10, 10);
                                 tv3.setTextSize(12);
-                                tv3.setWidth(tableWidth);
+                                tv3.setWidth(columnWidth);
 
+                                //adds it to the table row in the third column
                                 tr.addView(tv3, 2);
 
+                                //sets the fourth text view with the unit data for the exercise
                                 tv4.setText(unit);
                                 tv4.setTextColor(Color.BLACK);
                                 tv4.setPadding(10, 10, 10, 10);
                                 tv4.setTextSize(12);
-                                tv4.setWidth(tableWidth);
+                                tv4.setWidth(columnWidth);
 
+                                //adds it to the table row in the fourth column
                                 tr.addView(tv4, 3);
 
+                                //adds the row to the table
                                 tl.addView(tr);
                             }
                         }
