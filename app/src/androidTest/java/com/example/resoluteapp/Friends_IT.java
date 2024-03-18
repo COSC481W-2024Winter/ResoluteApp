e2dc0a3a37fda24903bd1ce43e56035c8369db9d
Kailin Bedoway <kbedoway@emich.edu>
2024-03-18 14:42:18 -0400

    update: Testing for Friends page
    This commit adds two new instrumented tests for the friends page. The removeOption() test relies on testUser2 existing in the friends_testUser1 collection. The removeFriend() test relies on this as well as testUser1 existing in the friends_testUser2 collection.
    
    * added removeOption() test in Friends_IT.java to check if user clicks table row of friends table, remove option will show
    * added removeFriend() test in Friends_IT.java to check if user clicks remove option, friend will be removed from user's friends collection and user will be removed from friend's friends collection
    
    issue #48

--- app/src/androidTest/java/com/example/resoluteapp/Friends_IT.java
+++ app/src/androidTest/java/com/example/resoluteapp/Friends_IT.java
@@ -4,6 +4,7 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
@@ -28,8 +29,8 @@ public class Friends_IT {
 
     @Test
     public void enterUsernameClears() throws InterruptedException {
-        onView(withId(R.id.login_username_entry)).perform(typeText("User1"), closeSoftKeyboard());
-        onView(withId(R.id.login_password_entry)).perform(typeText("Password1"), closeSoftKeyboard());
+        onView(withId(R.id.login_username_entry)).perform(typeText("testUser1"), closeSoftKeyboard());
+        onView(withId(R.id.login_password_entry)).perform(typeText("testUser1"), closeSoftKeyboard());
 
         onView(withId(R.id.login_button)).perform(click());
 
@@ -39,7 +40,7 @@ public class Friends_IT {
 
         Thread.sleep(1000);
 
-        onView(withId(R.id.enter_username)).perform(typeText("User10"), closeSoftKeyboard());
+        onView(withId(R.id.enter_username)).perform(typeText("testUser2"), closeSoftKeyboard());
         onView(withId(R.id.send_request)).perform(click());
 
         onView(withId(R.id.enter_username)).check(matches(withText("")));
@@ -47,8 +48,8 @@ public class Friends_IT {
 
     @Test
     public void generateFriendsTable() throws InterruptedException {
-        onView(withId(R.id.login_username_entry)).perform(typeText("User1"), closeSoftKeyboard());
-        onView(withId(R.id.login_password_entry)).perform(typeText("Password1"), closeSoftKeyboard());
+        onView(withId(R.id.login_username_entry)).perform(typeText("testUser1"), closeSoftKeyboard());
+        onView(withId(R.id.login_password_entry)).perform(typeText("testUser1"), closeSoftKeyboard());
 
         onView(withId(R.id.login_button)).perform(click());
 
@@ -59,5 +60,71 @@ public class Friends_IT {
         Thread.sleep(1000);
 
         onView(withId(R.id.friends_table)).check(matches(isDisplayed()));
+    }
+
+    //Relies on testUser2 existing in friends_testUser1 collection
+    @Test
+    public void removeOption() throws InterruptedException {
+        onView(withId(R.id.login_username_entry)).perform(typeText("testUser1"), closeSoftKeyboard());
+        onView(withId(R.id.login_password_entry)).perform(typeText("testUser1"), closeSoftKeyboard());
+
+        onView(withId(R.id.login_button)).perform(click());
+
+        Thread.sleep(1000);
+
+        onView(withId(R.id.to_friends_from_home)).perform(click());
+
+        Thread.sleep(1000);
+
+        onView(withText("testUser2")).perform(click());
+
+        Thread.sleep(1000);
+
+        onView(withText("Remove")).check(matches(withText("Remove")));
+    }
+
+    //Relies on testUser2 existing in friends_testUser1 collection
+    //Relies on testUser1 existing in friends_testUser2 collection
+    @Test
+    public void removeFriend() throws InterruptedException {
+        onView(withId(R.id.login_username_entry)).perform(typeText("testUser1"), closeSoftKeyboard());
+        onView(withId(R.id.login_password_entry)).perform(typeText("testUser1"), closeSoftKeyboard());
+
+        onView(withId(R.id.login_button)).perform(click());
+
+        Thread.sleep(1000);
+
+        onView(withId(R.id.to_friends_from_home)).perform(click());
+
+        Thread.sleep(1000);
+
+        onView(withText("testUser2")).perform(click());
+
+        Thread.sleep(1000);
+
+        onView(withText("Remove")).perform(click());
+
+        Thread.sleep(1000);
+
+        onView(withId(R.id.to_profile_from_friends)).perform(click());
+
+        Thread.sleep(1000);
+
+        onView(withId(R.id.logout_button)).perform(click());
+
+        Thread.sleep(1000);
+
+        onView(withId(R.id.login_username_entry)).perform(typeText("testUser2"), closeSoftKeyboard());
+        onView(withId(R.id.login_password_entry)).perform(typeText("testUser2"), closeSoftKeyboard());
+
+        onView(withId(R.id.login_button)).perform(click());
+
+        Thread.sleep(1000);
+
+        onView(withId(R.id.to_friends_from_home)).perform(click());
+
+        Thread.sleep(1000);
+
+        onView(withText("testUser1")).check(doesNotExist());
     }
 }
