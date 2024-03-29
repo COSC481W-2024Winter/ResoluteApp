4b0f3204314510c74bf29d9e5bcb3cc9d637a4a0
Jon Taylor <jtayl190@emich.edu>
2024-03-28 21:17:31 -0400

    Update: Added method to write to friends Inbox
    This commit takes care of the writing to a friends inbox a copy of the exercise logged
    
    * added sendExercisesToFriends method
    * modified logExercise method to call sendExercises
    
    Addresses Issue #44

--- app/src/main/java/com/example/resoluteapp/LogExerciseFragment.java
+++ app/src/main/java/com/example/resoluteapp/LogExerciseFragment.java
@@ -3,6 +3,7 @@ package com.example.resoluteapp;
 import static android.content.ContentValues.TAG;
 
 import android.annotation.SuppressLint;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,6 +18,7 @@ import androidx.navigation.fragment.NavHostFragment;
 import com.example.resoluteapp.databinding.FragmentLogExerciseBinding;
 import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -86,7 +88,7 @@ public class LogExerciseFragment extends Fragment {
     private void logExercise() {
 
         //get the username of the user that is currently logged in
-        String currentUser = ((MainActivity)getActivity()).getUsername();;
+        String currentUser = ((MainActivity) getActivity()).getUsername();
 
         String exerciseName = binding.editTextExercise.getText().toString().trim();
         String units = binding.editTextUnits.getText().toString().trim();
@@ -104,6 +106,7 @@ public class LogExerciseFragment extends Fragment {
         theExercise.put("Amount", numberOfUnits);
         theExercise.put("Date", Timestamp.now());
         theExercise.put("Date_String", dateString);
+        theExercise.put("Username", currentUser);
 
         theDB.collection("exercises_" + currentUser).add(theExercise).addOnSuccessListener(documentReference -> {
             // Log success
@@ -112,9 +115,10 @@ public class LogExerciseFragment extends Fragment {
             successMessage.show();
 
             // Clear EditText fields
-            binding.editTextExercise.setText("");
-            binding.editTextUnits.setText("");
-            binding.editTextNumberOfUnits.setText("");
+            clearFields();
+
+            sendExercisesToFriends(currentUser, theExercise);
+
 
         }).addOnFailureListener(e -> {
             // Log failure
@@ -131,6 +135,50 @@ public class LogExerciseFragment extends Fragment {
         binding.editTextExercise.setText("");
         binding.editTextUnits.setText("");
         binding.editTextNumberOfUnits.setText("");
+    }
+
+    private void sendExercisesToFriends(String currentUser, Map<String, Object> theExercise) {
+
+        // first Grab all the current users friends from the db
+
+        // set a listener to make sure theres a collection to grab friends from
+        theDB.collection("friends_" + currentUser).get().addOnCompleteListener(grabFriendsList -> {
+
+            // if we've found a collection and there are friends to write to
+            if (grabFriendsList.isSuccessful() && grabFriendsList.getResult() != null) {
+
+                // for every friend in a users friend list
+                for (QueryDocumentSnapshot friend : grabFriendsList.getResult()) {
+
+                    // grab the username for the friend
+                    String friendsUsername = friend.getString("username");
+
+                    // make sure they have a username field
+                    if (friendsUsername != null) {
+
+                        // Add the exercise to their collection
+                        theDB.collection("inbox_" + friendsUsername).add(theExercise)
+                                // with logs for if it was successful or not
+                                .addOnSuccessListener(documentReference -> Log.d(TAG, "Exercise sent to friend: " + friendsUsername))
+                                .addOnFailureListener(error -> Log.d(TAG, "Error Sending Exercise to Friend: " + friendsUsername));
+                    } else {
+
+                        // the friend isnt set up right? how did we get here
+                        Log.w(TAG, "Friend exists but doesn't?");
+
+                    }
+
+                }
+
+            } else {
+                // Friends list doesn't exist OR the user doesnt have friends
+                Log.w(TAG, "No Friends Yet");
+            }
+
+
+        });
+
+
     }
 
     @Override
