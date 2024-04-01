c4c4e5c8d7e1f01e689eb20fb648bae81b6bad43
Jonathan <jtayl190@emich.edu>
2024-04-01 12:31:15 -0400

    Update: Added Testing for Sending Exercises to Inboxes
    * added sendExerciseToFriend() that logs an exercise and logs into the friends page to verify the exercise exists
    
    Addresses Issue #44

--- app/src/androidTest/java/com/example/resoluteapp/LogExercise_IT.java
+++ app/src/androidTest/java/com/example/resoluteapp/LogExercise_IT.java
@@ -5,18 +5,17 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
-import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.Matchers.allOf;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import androidx.test.espresso.NoMatchingViewException;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -37,8 +36,8 @@ public class LogExercise_IT {
     @Before
     public void navigateToLogExerciseScreen() throws InterruptedException {
         // Navigate to the log exercise screen before each test
-        onView(withId(R.id.login_username_entry)).perform(typeText("User1"), closeSoftKeyboard());
-        onView(withId(R.id.login_password_entry)).perform(typeText("Password1"), closeSoftKeyboard());
+        onView(withId(R.id.login_username_entry)).perform(typeText("Taylor.Jon"), closeSoftKeyboard());
+        onView(withId(R.id.login_password_entry)).perform(typeText("Password"), closeSoftKeyboard());
         onView(withId(R.id.login_button)).perform(click());
         // You may need to add a delay here if navigation is asynchronous
         Thread.sleep(1000);
@@ -48,7 +47,7 @@ public class LogExercise_IT {
 
     //This function clears the SharedPreferences file after every test
     @After
-    public void emptySharedPref(){
+    public void emptySharedPref() {
         SharedPreferences sp = getInstrumentation().getTargetContext().getSharedPreferences("com.example.ResoluteApp.SharedPrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
         editor.clear();
@@ -85,4 +84,38 @@ public class LogExercise_IT {
         onView(withId(R.id.editTextNumberOfUnits)).check(matches(withText("")));
 
     }
+
+    @Test
+    public void sendExerciseToFriend() throws InterruptedException {
+
+        // Log an exercise
+        onView(withId(R.id.editTextExercise)).perform(typeText("UNIQUE"));
+        onView(withId(R.id.editTextUnits)).perform(typeText("Units"));
+        onView(withId(R.id.editTextNumberOfUnits)).perform(typeText("Amounts"), closeSoftKeyboard());
+        onView(withId(R.id.log_exercise_button)).perform(click());
+        Thread.sleep(2000);
+
+        // Log Out and Relaunch
+        emptySharedPref();
+        activityScenarioRule.getScenario().close();
+        ActivityScenario.launch(MainActivity.class, null);
+
+        // Log In As Friend
+        onView(withId(R.id.login_username_entry)).perform(typeText("Friend"), closeSoftKeyboard());
+        onView(withId(R.id.login_password_entry)).perform(typeText("Password"), closeSoftKeyboard());
+        onView(withId(R.id.login_button)).perform(click());
+
+        // Navigate to Inbox
+        Thread.sleep(2000);
+        onView(withId(R.id.to_inbox_from_home)).perform(click());
+        Thread.sleep(2000);
+
+        onView(withId(R.id.tableLayout)).check(matches(isDisplayed()));
+        onView(allOf(withText("UNIQUE"))).perform(click());
+
+
+
+    }
+
+
 }
